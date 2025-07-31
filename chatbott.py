659cf773f5f393607f78d17a3edc314963659f66
ayu-ctrl <ayushi241006@gmail.com>
2025-07-31 20:21:16 +0530

    Add files via upload

---
+++ chatbott.py
@@ -0,0 +1,83 @@
+import random
+import json
+import pickle
+import numpy as np
+import nltk
+import tensorflow as tf
+
+from nltk.stem import WordNetLemmatizer
+from keras.models import load_model
+lemmatizer = WordNetLemmatizer()
+import json
+
+with open("intents.json", "r") as file:
+    intents = json.load(file)
+
+print (intents)
+import nltk
+nltk.data.path.append('/path/to/your/nltk_data')
+import re
+import pickle
+from nltk.stem import WordNetLemmatizer
+
+lemmatizer = WordNetLemmatizer()
+
+# Sample intents (replace with your own)
+intents = {
+    "intents": [
+        {"tag": "greeting", "patterns": ["Hello", "Hi there!", "How are you?"]},
+        {"tag": "goodbye", "patterns": ["Bye", "See you", "Goodbye!"]}
+    ]
+}
+
+words = []
+classes = []
+documents = []
+ignoreLetters = ['?', '!', '.', ',']
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        wordList = re.findall(r'\b\w+\b', pattern.lower())  # 🔁 Using re instead of nltk
+        words.extend(wordList)
+        documents.append((wordList, intent['tag']))
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+words = [lemmatizer.lemmatize(word) for word in words if word not in ignoreLetters]
+words = sorted(set(words))
+classes = sorted(set(classes))
+
+pickle.dump(words, open('words.pkl', 'wb'))
+pickle.dump(classes, open('classes.pkl', 'wb'))
+
+# Starting training set creation
+training = []
+outputEmpty = [0] * len(classes)
+
+for document in documents:
+    bag = []
+    wordPatterns = document[0]
+    wordPatterns = [lemmatizer.lemmatize(word.lower()) for word in wordPatterns]
+    for word in words:
+        bag.append(1) if word in wordPatterns else bag.append(0)
+
+    outputRow = list(outputEmpty)
+    outputRow[classes.index(document[1])] = 1
+    training.append(bag + outputRow)
+    random.shuffle(training)
+training = np.array(training)
+trainX = training[:, :len(words)]
+trainY = training[:, len(words):]
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Dense(128, input_shape=(len(trainX[0]),), activation = 'relu'))
+model.add(tf.keras.layers.Dropout(0.5))
+model.add(tf.keras.layers.Dense(64, activation = 'relu'))
+model.add(tf.keras.layers.Dropout(0.5))
+model.add(tf.keras.layers.Dense(len(trainY[0]), activation='softmax'))
+
+sgd = tf.keras.optimizers.SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+hist = model.fit(np.array(trainX), np.array(trainY), epochs=200, batch_size=5, verbose=1)
+model.save('chatbot_model.h5', hist)
+print('Done')
